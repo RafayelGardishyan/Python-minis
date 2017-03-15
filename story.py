b75628e5e6c142df1586ecbcc100772108a6a1c4
RafayelGardishyan <rgardishyan@gmail.com>
2017-03-15 16:05:54 +0100

    We gaan een verhaaltje maken
    Volg de instructies en krijg lol!!!

--- story.py
+++ story.py
@@ -1,9 +1,9 @@
 print ("We gaan een verhaaltje maken! Volg de instructies")
-w1 = input("Zelfstandig naamwoord")
-w2 = input('Ander zelfstandig naamwoord')
-w3 = input("Werkwoord")
-w4 = input("Ander werkwoord")
-w5 = input("Bijvoeglijk naamwoord")
-w6 = input("Ander bijvoeglijk naamwoord")
+w1 = input("Zelfstandig naamwoord :")
+w2 = input('Ander zelfstandig naamwoord :')
+w3 = input("Werkwoord :")
+w4 = input("Ander werkwoord :")
+w5 = input("Bijvoeglijk naamwoord :")
+w6 = input("Ander bijvoeglijk naamwoord :")
 
-print ('De' + " " + w5 + " " + w1 + " " + 'Ging' + " " + w3 + ' met ' + w6 + " " + w2 + " om te " + w4 + " " )
+print ('De' + " " + w5 + " " + w1 + " " + 'Ging' + " " + w3 + ' met ' + w6 + " " + w2 + " om te " + w4 + " " 
